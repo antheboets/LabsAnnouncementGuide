d0539cb995a4f1f9efeaf69080fcab987aa33063
antheboets <anthe.boets@student.ehb.be>
2022-05-06 23:29:22 +0200

    adding localization and header to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
+import Header from './components/Header.js';
+import LocalizedStrings from 'react-localization';
+import Localization from './localization/Localization';
+
 import './App.css';
 
 function App() {
+
+  const strings = new LocalizedStrings(Localization);
+  strings.setLanguage("en");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header strings={strings}/>
     </div>
   );
 }
