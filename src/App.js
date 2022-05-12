6eecc4f1a424a04968d05b80e5422dacd5d095ed
antheboets <anthe.boets@student.ehb.be>
2022-05-12 17:53:23 +0200

    removing old localization code in app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import Header from './components/Header.js';
 import Footer from './components/Footer';
+/*
+old localization
 import LocalizedStrings from 'react-localization';
 import Localization from './localization/Localization';
-
+*/
 import './App.css';
 import React from 'react';
 
 function App() {
-
-  const [strings, setStrings] = React.useState(new LocalizedStrings(Localization));
+  /*
+  old localization
+  //const [strings, setStrings] = React.useState(new LocalizedStrings(Localization));
   //const strings = new LocalizedStrings(Localization);
 
 
@@ -17,14 +20,22 @@ function App() {
     console.log(code,strings);
     strings.setLanguage("Nl")
     console.log(strings.getLanguage());
-    setStrings(strings.setLanguage("Nl"))
+    //setStrings(strings)
   }
-
-
+  */
+  /*
+  old localization
   return (
     <div onClick={() =>{setLanguage("Nl")}} className="App">
       <Header strings={strings} setLanguage={setLanguage}/>
       <Footer strings={strings}/>
+    </div>
+  );
+  */
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
     </div>
   );
 }
